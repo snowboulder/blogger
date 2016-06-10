c233e04c6d17b17e4ad95b319d4be2223eaee780
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-06-10 18:22:37 -0400

    Add Paperclip Gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 
 end
